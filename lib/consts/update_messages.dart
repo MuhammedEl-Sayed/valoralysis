be4675b407924d6396a0091ed171a288aa26e476
Muhammed El-Sayed <g4mereaper@gmail.com>
2024-05-16 00:35:36 -0400

    Comma

--- lib/consts/update_messages.dart
+++ lib/consts/update_messages.dart
@@ -1,5 +1,5 @@
 const updateMessages = [
   "- Added KAST score calculation.",
-  "- Fixed mismatched column header background size."
-      "- Fixed some issues with matches preventing the table from showing."
+  "- Fixed mismatched column header background size.",
+  "- Fixed some issues with matches preventing the table from showing."
 ];
