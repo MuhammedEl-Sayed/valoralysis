c72eadbac631aa3fee62ab19ce65560fe4c90f1a
melsayed <g4mereaper@gmail.com>
2024-05-26 20:49:51 -0400

    Added ADR to charts.

--- lib/consts/update_messages.dart
+++ lib/consts/update_messages.dart
@@ -1,4 +1,5 @@
 const updateMessages = [
   "- Updated with a new and improved table! Removed rank names and made rank icons more prominent.",
-  "- Added KD to the history tiles."
+  "- Added KD to the history tiles.",
+  "- Added ADR to the match chart.",
 ];
